e0ecdeb9f9ac9d365d00817ad18b2d5808b79c0a
raj shringi <raj24shringi@gmail.com>
2022-10-18 23:37:30 +0530

    create second navbar when user is logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
+import { MdOutlineLibraryAdd, MdSettings, MdHome } from "react-icons/md";
 
-function Header() {
+function Header({ token }) {
   return (
     <div className="py-4 shadow-sm bg-white box-border">
       <header className="container mx-auto">
@@ -10,23 +11,60 @@ function Header() {
               Conduit
             </h3>
           </NavLink>
-          <ul className="flex justify-between items-center">
-            <NavLink exact to="/" activeClassName="text-indigo-400">
-              <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400">
-                Home
-              </li>
-            </NavLink>
-            <NavLink to="/login" activeClassName="text-indigo-400">
-              <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400">
-                Sign in
-              </li>
-            </NavLink>
-            <NavLink to="/signup" activeClassName="text-indigo-400">
-              <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400">
-                Sign up
-              </li>
-            </NavLink>
-          </ul>
+
+          {!token && (
+            <ul className="flex justify-between items-center">
+              <NavLink exact to="/" activeClassName="text-indigo-400">
+                <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400 flex items-center space-x-2">
+                  <MdHome className="text-2xl" />
+                  <span>Home</span>
+                </li>
+              </NavLink>
+
+              <NavLink to="/login" activeClassName="text-indigo-400">
+                <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400">
+                  Sign in
+                </li>
+              </NavLink>
+
+              <NavLink to="/signup" activeClassName="text-indigo-400">
+                <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400">
+                  Sign up
+                </li>
+              </NavLink>
+            </ul>
+          )}
+
+          {token && (
+            <ul className="flex justify-between items-center">
+              <NavLink exact to="/" activeClassName="text-indigo-400">
+                <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400 flex items-center space-x-2">
+                  <MdHome className="text-2xl" />
+                  <span>Home</span>
+                </li>
+              </NavLink>
+
+              <NavLink exact to="*" activeClassName="text-indigo-400">
+                <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400 flex items-center space-x-2">
+                  <MdOutlineLibraryAdd className="text-2xl" />
+                  <span>New Post</span>
+                </li>
+              </NavLink>
+
+              <NavLink exact to="*" activeClassName="text-indigo-400">
+                <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400 flex items-center space-x-2">
+                  <MdSettings className="text-2xl" />
+                  <span>Home</span>
+                </li>
+              </NavLink>
+
+              <NavLink exact to="*" activeClassName="text-indigo-400">
+                <li className="mx-6 p-2 cursor-pointer border-b-2 border-transparent hover:border-indigo-400">
+                  user
+                </li>
+              </NavLink>
+            </ul>
+          )}
         </nav>
       </header>
     </div>
