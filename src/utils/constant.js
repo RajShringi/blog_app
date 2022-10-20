5063080f82bcc6ae5eb5f09fdb0e8775da4eb5df
raj shringi <raj24shringi@gmail.com>
2022-10-20 15:58:58 +0530

    create singup and login url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,5 @@
 const ROOT_URL = "https://mighty-oasis-08080.herokuapp.com/api";
 const articleURL = ROOT_URL + "/articles";
-export { ROOT_URL, articleURL };
+const singupURL = ROOT_URL + "/users";
+const loginURL = ROOT_URL + "/users/login";
+export { ROOT_URL, articleURL, singupURL, loginURL };
