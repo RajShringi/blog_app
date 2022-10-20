a4986a07c4dc7076f9d5eb6874261301b903159b
raj shringi <raj24shringi@gmail.com>
2022-10-20 21:25:41 +0530

    add local storage key and user verify url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,4 +2,14 @@ const ROOT_URL = "https://mighty-oasis-08080.herokuapp.com/api";
 const articleURL = ROOT_URL + "/articles";
 const singupURL = ROOT_URL + "/users";
 const loginURL = ROOT_URL + "/users/login";
-export { ROOT_URL, articleURL, singupURL, loginURL };
+const localStorageKey = "app_user";
+const userVerify = ROOT_URL + "/user";
+
+export {
+  ROOT_URL,
+  articleURL,
+  singupURL,
+  loginURL,
+  localStorageKey,
+  userVerify,
+};
